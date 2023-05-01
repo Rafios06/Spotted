585de27ba298716ffa3fd254138f58a89ba09ab7
Rafael <131557669+Rafios06@users.noreply.github.com>
2023-05-01 21:21:14 +0200

    Add files via upload

---
+++ httpdocs/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Spotted - Welcome</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+</head>
+
+<body>
+
+    <section class="hero is-fullheight has-background-white-bis">
+        <!-- Hero head: will stick at the top -->
+        <div class="hero-head">
+            <header class="navbar">
+                <div class="container">
+                    <div class="navbar-brand">
+                        <a class="navbar-item">
+                            <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+                        </a>
+                        <span class="navbar-burger" data-target="navbarMenuHeroC">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </span>
+                    </div>
+                    <div id="navbarMenuHeroC" class="navbar-menu">
+                        <div class="navbar-end">
+                            <span class="navbar-item">
+                                <a class="button is-success">
+                                    <span>Log in</span>
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+
+        <!-- Hero content: will be in the middle -->
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <p class="title">
+                    Title
+                </p>
+
+                <p class="subtitle">
+                    Title
+                </p>
+
+            </div>
+        </div>
+
+        <!-- Hero footer: will stick at the bottom -->
+        <footer class="hero-foot column">
+            <div class="content has-text-centered">
+                <p>
+                    <strong>Spotted</strong> by <a href="https://jgthms.com">Rafaël Sanchez</a>. The source code is licensed
+                    <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The website content
+                    is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+                </p>
+            </div>
+        </footer>
+    </section>
+
+</body>
+
+</html>
