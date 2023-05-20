de12e96da9f19f56d6e16b17ba69953a65b6393f
Rafael <131557669+Rafios06@users.noreply.github.com>
2023-05-21 00:38:53 +0200

    Add files via upload

--- httpdocs/newsignal.php
+++ httpdocs/newsignal.php
@@ -56,47 +56,68 @@ require("configsql.php");
     </nav>
 
     <div class="card" style="margin: 2em;">
-        <div class="card-content has-text-centered">
+        <div class="card-content">
             <form action="addsignal.php" method="POST">
-                <div>
-                    <label for="sfrequency">Frequency</label>
-                    <input type="text" name="sfrequency" id="sfrequency" required>
-                    <select name="sfrequencyunit" id="sfrequencyunit">
-                        <option value="GHz">GHz</option>
-                        <option value="MHz" selected>MHz</option>
-                        <option value="kHz">kHz</option>
-                        <option value="Hz">Hz</option>
-                    </select>
 
+                <label class="label" for="sfrequency">Frequency</label>
+                <div class="field has-addons">
+                    <p class="control">
+                        <input class="input" type="text" placeholder="Frequency" name="sfrequency" id="sfrequency" required>
+                    </p>
+                    <p class="control">
+                        <span class="select">
+                            <select name="sfrequencyunit" id="sfrequencyunit">
+                                <option value="GHz">GHz</option>
+                                <option value="MHz" selected>MHz</option>
+                                <option value="kHz">kHz</option>
+                                <option value="Hz">Hz</option>
+                            </select>
+                        </span>
+                    </p>
                 </div>
 
                 <div>
-                    <label for="stime">Time (UTC)</label>
-                    <input type="text" name="stime" id="stime" required>
+                    <label class="label" for="stime">Time (UTC)</label>
+                    <input class="input" type="datetime-local" name="stime" id="stime" value="<?= date('Y-m-d H:i') ?>" required>
                 </div>
 
                 <div>
-                    <label for="sreceiver">Receiver</label>
-                    <input type="text" name="sreceiver" id="sreceiver" required>
+                    <label class="label" for="sreceiver">Receiver</label>
+                    <input class="input" type="text" name="sreceiver" id="sreceiver" required>
+                </div>
+
+                <div class="field has-addons">
+                    <p class="control">
+                        <span class="select">
+                            <select name="sreceiver" id="sreceiver">
+                                <option value="----" selected>Create new receiver</option>
+                                <option value="MHz">Home (SDRPLAY)</option>
+                                <option value="kHz">WEBSDR - University</option>
+                            </select>
+                        </span>
+                    </p>
+                    <p class="control">
+                        <button class="button">Create</button>
+                    </p>
                 </div>
 
                 <div>
-                    <label for="snoise">S/N</label>
-                    <input type="text" name="snoise" id="snoise" required>
+                    <label class="label" for="snoise">S/N</label>
+                    <input class="input" type="text" name="snoise" id="snoise" required>
                 </div>
 
                 <div>
-                    <label for="scomment">Comment</label>
-                    <input type="text" name="scomment" id="scomment" required>
+                    <label class="label" for="scomment">Comment</label>
+                    <input class="input" type="text" name="scomment" id="scomment" required>
                 </div>
 
                 <div>
-                    <label for="slink">Link sample</label>
-                    <input type="text" name="slink" id="slink" required>
+                    <label class="label" for="slink">Link sample</label>
+                    <input class="input" type="text" name="slink" id="slink" required>
                 </div>
 
                 <div>
-                    <label for="sprivate">Private</label>
+                    <label class="label" for="sprivate">Private</label>
                     <input type="checkbox" name="sprivate" id="sprivate">
                 </div>
 
