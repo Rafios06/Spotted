9b93a6995006508648408f041bab39a4061f0e8e
Rafael <131557669+Rafios06@users.noreply.github.com>
2023-06-27 23:45:28 +0200

    Add files via upload

--- newreceiver.php
+++ newreceiver.php
@@ -27,8 +27,8 @@ if (!empty($_GET['id'])) {
 <head>
     <meta charset="UTF-8">
     <?php
-    echo 'xx=' . var_dump($editorMode);
-    if ($editorMode) {
+
+if ($editorMode) {
         echo '<title>Spotted - Edit receiver</title>';
     } else {
         echo '<title>Spotted - Add receiver</title>';
@@ -80,55 +80,31 @@ if (!empty($_GET['id'])) {
     <div class="card" style="margin: 2em;">
         <div class="card-content">
 
-        <?php if($editorMode){echo `
-            <form action="addreceiver.php" method="POST">
+            <form action="<?php if ($editorMode) {echo 'editreceiver.php';} else {echo 'addreceiver.php';} ?>" method="POST">
+                <?php if ($editorMode) {echo '<input type="hidden" id="receiverId" name="receiverId" value="'.$receiverId.'" />';} ?>
+                
                 <div>
                     <label class="label" for="rtitle">Title</label>
-                    <input class="input" type="text" name="rtitle" id="rtitle" value="`.<?php echo $receiverDetails['title']; ?>." required>
+                    <input class="input" type="text" name="rtitle" id="rtitle" value="<?php if ($editorMode) {echo $receiverDetails['title'];} ?>" required>
                 </div>
 
                 <div>
                     <label class="label" for="rlocation">Location</label>
-                    <input class="input" type="text" name="rlocation" id="rlocation" value="<?php echo $receiverDetails['location']; ?>" required>
+                    <input class="input" type="text" name="rlocation" id="rlocation" value="<?php if ($editorMode) {echo $receiverDetails['location'];} ?>" required>
                 </div>
 
                 <div>
                     <label class="label" for="rdevice">Device</label>
-                    <input class="input" type="text" name="rdevice" id="rdevice" value="<?php echo $receiverDetails['device']; ?>" required>
+                    <input class="input" type="text" name="rdevice" id="rdevice" value="<?php if ($editorMode) {echo $receiverDetails['device'];} ?>" required>
                 </div>
 
                 <div>
                     <label class="label" for="rantenna">Antenna</label>
-                    <input class="input" type="text" name="rantenna" id="rantenna" value="<?php echo $receiverDetails['antenna']; ?>" required>
+                    <input class="input" type="text" name="rantenna" id="rantenna" value="<?php if ($editorMode) {echo $receiverDetails['antenna'];} ?>" required>
                 </div>
 
-                <input style="margin-top: 1em;" class="button" type="submit" value="Add">
+                <input style="margin-top: 1em;" class="button" type="submit" <?php if ($editorMode) {echo 'value="Change"';} else {echo 'value="Add"';} ?> >
             </form>
-            `;} else{echo `
-                <form action="addreceiver.php" method="POST">
-                    <div>
-                        <label class="label" for="rtitle">Title</label>
-                        <input class="input" type="text" name="rtitle" id="rtitle" required>
-                    </div>
-    
-                    <div>
-                        <label class="label" for="rlocation">Location</label>
-                        <input class="input" type="text" name="rlocation" id="rlocation" required>
-                    </div>
-    
-                    <div>
-                        <label class="label" for="rdevice">Device</label>
-                        <input class="input" type="text" name="rdevice" id="rdevice" required>
-                    </div>
-    
-                    <div>
-                        <label class="label" for="rantenna">Antenna</label>
-                        <input class="input" type="text" name="rantenna" id="rantenna" required>
-                    </div>
-    
-                    <input style="margin-top: 1em;" class="button" type="submit" value="Add">
-                </form>
-                `;}?>
 
         </div>
     </div>
